75eed53bfe9f11cad6910d5d06f2a5e3e96fb540
brafdlog <brafdlog@gmail.com>
2015-01-21 00:20:08 +0200

    Add spring

--- src/main/java/com/fantasy/government/rest/TransparentPoliticsServiceRest.java
+++ src/main/java/com/fantasy/government/rest/TransparentPoliticsServiceRest.java
@@ -5,9 +5,12 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import org.springframework.stereotype.Component;
+
 import com.fantasy.government.rest.data.GovMembersListView;
 import com.fantasy.government.services.proxy.OpenKnessetProxy;
 
+@Component
 @Path("/proxy/openknesset")
 @Produces(MediaType.APPLICATION_JSON)
 public class TransparentPoliticsServiceRest {
