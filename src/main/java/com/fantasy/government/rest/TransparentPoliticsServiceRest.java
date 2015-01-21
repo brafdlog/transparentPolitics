1b1cf1d81390868f8dcace9f38fbdc6e52719850
Emmanuel Alhadeff <emmanuel@dudamobile.com>
2015-01-21 15:54:13 +0200

    Changed RESTEasy to SpringMVC

--- src/main/java/com/fantasy/government/rest/TransparentPoliticsServiceRest.java
+++ src/main/java/com/fantasy/government/rest/TransparentPoliticsServiceRest.java
@@ -1,26 +1,19 @@
 package com.fantasy.government.rest;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.fantasy.government.rest.data.GovMembersListView;
 import com.fantasy.government.services.proxy.OpenKnessetProxy;
 
-@Component
-@Path("/proxy/openknesset")
-@Produces(MediaType.APPLICATION_JSON)
+@RestController
 public class TransparentPoliticsServiceRest {
 	
 	@Autowired
 	OpenKnessetProxy proxy;
 	
-	@GET
-	@Path("/members")
+	@RequestMapping("/members")
 	public GovMembersListView getMembers() {
 		return proxy.getMembers();
 	}
