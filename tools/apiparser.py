ad1ea9c271785c8d2e5bba38c402fbfb065b4e14
Emmanuel Alhadeff <emmanuel@dudamobile.com>
2015-01-22 14:43:53 +0200

    Tool improved

--- tools/apiparser.py
+++ tools/apiparser.py
@@ -3,11 +3,13 @@ from urllib import request
 from jinja2 import Environment, PackageLoader
 from termcolor import colored
 import json
+import sys
 
 class KnessetAPIParser():
 	def __init__(self):
 		env = Environment(loader=PackageLoader('apiparser', '.'))
 		env.filters['firstcase'] = KnessetAPIParser.firstCase
+		env.filters['modeltype'] = self.translateType
 		self.__template__ = env.get_template('class.java.tmpl')
 
 		self.__typeMap__ = {
@@ -17,6 +19,7 @@ class KnessetAPIParser():
 			'float': "Float",
 			'string': "String",
 			'related': "List<String>",
+			'list': "List<String>",
 			'datetime': "java.util.Date"
 		}
 
@@ -24,35 +27,58 @@ class KnessetAPIParser():
 	def firstCase(value):
 		return value[0:1].upper() + value[1:]
 
+	def translateType(self, value):
+		if value in self.__typeMap__:
+			return self.__typeMap__[value]
+
+		return KnessetAPIParser.firstCase(value)
+
+	def discoverNewType(self, modelName, model):
+		mtype = model['type']
+		if mtype.lower() == 'list':
+			if 'items' in model:
+				newType = KnessetAPIParser.firstCase(model['items']['$ref'])
+				self.__typeMap__[modelName] = "List<" + newType + ">"
+				return True
+		return False
+
 	def parseSchema(self, resource):
 		response = request.urlopen('https://oknesset.org/api/v2/doc/schema/' + resource).read().decode('utf-8')
 		j = json.loads(response)
 
+		models = []
 		for key in j['models']:
-			self.parseModel(key, j['models'][key])
+			newModel = self.parseModel(key, j['models'][key])
+			if (newModel):
+				models.append(newModel)
+
+		for m in models:
+			print(self.__template__.render(**m))
 
 	def parseModel(self, modelName, modelMap):
 		members = []
 
 		properties = modelMap['properties']
 		for key in properties:
-			m = {'name': key, 'type': self.parseModelType(properties[key])}
-			members.append(m)
+			if (not self.discoverNewType(modelName, properties[key])):
+				m = {'name': key, 'type': properties[key]['type']}
+				members.append(m)
 
-		print(self.__template__.render(class_name=modelName, class_members=members))
-
-	def parseModelType(self, model):
-		mtype = model['type']
-		if mtype.lower() == 'list':
-			if 'items' in model:
-				return "List<" + KnessetAPIParser.firstCase(model['items']['$ref']) + ">"
-			else:
-				return "List<String>"
-		elif mtype in self.__typeMap__:
-			return self.__typeMap__[mtype]
-
-		return KnessetAPIParser.firstCase(mtype)
+		if (members):
+			return { 'class_name' : modelName, 'class_members' : members }
+		else:
+			return None
 
 if __name__ == '__main__':
-	parser = KnessetAPIParser();
-	parser.parseSchema('member')
+	# retrieve argument mapping
+	args = {k: v for k,v in [arg.split('=') for arg in sys.argv[1:]]}
+	
+	if '--schema' in args:
+		parser = KnessetAPIParser();
+		parser.parseSchema(args['--schema'])
+	else:
+		print("""
+			Usage: {0} [--schema=SCHEMA]
+
+			- schema : The SCHEMA you'd like to generate JSON POJOs for
+			""".format(sys.argv[0]))
