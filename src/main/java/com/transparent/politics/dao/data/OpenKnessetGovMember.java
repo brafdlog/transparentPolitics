afd59840b3c65db361acb1ad730dc6dc11dee1a1
brafdlog <brafdlog@gmail.com>
2015-02-07 17:03:47 +0200

    Add hashcode and equals to OpenKnessetGovMember

--- src/main/java/com/transparent/politics/dao/data/OpenKnessetGovMember.java
+++ src/main/java/com/transparent/politics/dao/data/OpenKnessetGovMember.java
@@ -418,4 +418,269 @@ public class OpenKnessetGovMember implements GovMember {
         return id + " " + name;
     }
 
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((absolute_url == null) ? 0 : absolute_url.hashCode());
+        result = prime * result + ((agendas_uri == null) ? 0 : agendas_uri.hashCode());
+        result = prime * result + ((average_monthly_committee_presence == null) ? 0 : average_monthly_committee_presence.hashCode());
+        result = prime * result + ((average_weekly_presence_hours == null) ? 0 : average_weekly_presence_hours.hashCode());
+        result = prime * result + ((average_weekly_presence_rank == null) ? 0 : average_weekly_presence_rank.hashCode());
+        result = prime * result + ((bills_stats_approved == null) ? 0 : bills_stats_approved.hashCode());
+        result = prime * result + ((bills_stats_first == null) ? 0 : bills_stats_first.hashCode());
+        result = prime * result + ((bills_stats_pre == null) ? 0 : bills_stats_pre.hashCode());
+        result = prime * result + ((bills_stats_proposed == null) ? 0 : bills_stats_proposed.hashCode());
+        result = prime * result + ((bills_uri == null) ? 0 : bills_uri.hashCode());
+        result = prime * result + ((committees == null) ? 0 : committees.hashCode());
+        result = prime * result + ((current_position == null) ? 0 : current_position.hashCode());
+        result = prime * result + ((current_role_descriptions == null) ? 0 : current_role_descriptions.hashCode());
+        result = prime * result + ((date_of_birth == null) ? 0 : date_of_birth.hashCode());
+        result = prime * result + ((date_of_death == null) ? 0 : date_of_death.hashCode());
+        result = prime * result + ((detailed_roles == null) ? 0 : detailed_roles.hashCode());
+        result = prime * result + ((email == null) ? 0 : email.hashCode());
+        result = prime * result + ((end_date == null) ? 0 : end_date.hashCode());
+        result = prime * result + ((family_status == null) ? 0 : family_status.hashCode());
+        result = prime * result + ((fax == null) ? 0 : fax.hashCode());
+        result = prime * result + ((gender == null) ? 0 : gender.hashCode());
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        result = prime * result + ((img_url == null) ? 0 : img_url.hashCode());
+        result = prime * result + ((is_current == null) ? 0 : is_current.hashCode());
+        result = prime * result + ((links == null) ? 0 : links.hashCode());
+        result = prime * result + ((mmms_count == null) ? 0 : mmms_count.hashCode());
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        result = prime * result + ((number_of_children == null) ? 0 : number_of_children.hashCode());
+        result = prime * result + ((party_name == null) ? 0 : party_name.hashCode());
+        result = prime * result + ((party_url == null) ? 0 : party_url.hashCode());
+        result = prime * result + ((phone == null) ? 0 : phone.hashCode());
+        result = prime * result + ((place_of_birth == null) ? 0 : place_of_birth.hashCode());
+        result = prime * result + ((place_of_residence == null) ? 0 : place_of_residence.hashCode());
+        result = prime * result + ((place_of_residence_lat == null) ? 0 : place_of_residence_lat.hashCode());
+        result = prime * result + ((place_of_residence_lon == null) ? 0 : place_of_residence_lon.hashCode());
+        result = prime * result + ((residence_centrality == null) ? 0 : residence_centrality.hashCode());
+        result = prime * result + ((residence_economy == null) ? 0 : residence_economy.hashCode());
+        result = prime * result + ((resource_uri == null) ? 0 : resource_uri.hashCode());
+        result = prime * result + ((start_date == null) ? 0 : start_date.hashCode());
+        result = prime * result + ((votes_count == null) ? 0 : votes_count.hashCode());
+        result = prime * result + ((year_of_aliyah == null) ? 0 : year_of_aliyah.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        OpenKnessetGovMember other = (OpenKnessetGovMember) obj;
+        if (absolute_url == null) {
+            if (other.absolute_url != null)
+                return false;
+        } else if (!absolute_url.equals(other.absolute_url))
+            return false;
+        if (agendas_uri == null) {
+            if (other.agendas_uri != null)
+                return false;
+        } else if (!agendas_uri.equals(other.agendas_uri))
+            return false;
+        if (average_monthly_committee_presence == null) {
+            if (other.average_monthly_committee_presence != null)
+                return false;
+        } else if (!average_monthly_committee_presence.equals(other.average_monthly_committee_presence))
+            return false;
+        if (average_weekly_presence_hours == null) {
+            if (other.average_weekly_presence_hours != null)
+                return false;
+        } else if (!average_weekly_presence_hours.equals(other.average_weekly_presence_hours))
+            return false;
+        if (average_weekly_presence_rank == null) {
+            if (other.average_weekly_presence_rank != null)
+                return false;
+        } else if (!average_weekly_presence_rank.equals(other.average_weekly_presence_rank))
+            return false;
+        if (bills_stats_approved == null) {
+            if (other.bills_stats_approved != null)
+                return false;
+        } else if (!bills_stats_approved.equals(other.bills_stats_approved))
+            return false;
+        if (bills_stats_first == null) {
+            if (other.bills_stats_first != null)
+                return false;
+        } else if (!bills_stats_first.equals(other.bills_stats_first))
+            return false;
+        if (bills_stats_pre == null) {
+            if (other.bills_stats_pre != null)
+                return false;
+        } else if (!bills_stats_pre.equals(other.bills_stats_pre))
+            return false;
+        if (bills_stats_proposed == null) {
+            if (other.bills_stats_proposed != null)
+                return false;
+        } else if (!bills_stats_proposed.equals(other.bills_stats_proposed))
+            return false;
+        if (bills_uri == null) {
+            if (other.bills_uri != null)
+                return false;
+        } else if (!bills_uri.equals(other.bills_uri))
+            return false;
+        if (committees == null) {
+            if (other.committees != null)
+                return false;
+        } else if (!committees.equals(other.committees))
+            return false;
+        if (current_position == null) {
+            if (other.current_position != null)
+                return false;
+        } else if (!current_position.equals(other.current_position))
+            return false;
+        if (current_role_descriptions == null) {
+            if (other.current_role_descriptions != null)
+                return false;
+        } else if (!current_role_descriptions.equals(other.current_role_descriptions))
+            return false;
+        if (date_of_birth == null) {
+            if (other.date_of_birth != null)
+                return false;
+        } else if (!date_of_birth.equals(other.date_of_birth))
+            return false;
+        if (date_of_death == null) {
+            if (other.date_of_death != null)
+                return false;
+        } else if (!date_of_death.equals(other.date_of_death))
+            return false;
+        if (detailed_roles == null) {
+            if (other.detailed_roles != null)
+                return false;
+        } else if (!detailed_roles.equals(other.detailed_roles))
+            return false;
+        if (email == null) {
+            if (other.email != null)
+                return false;
+        } else if (!email.equals(other.email))
+            return false;
+        if (end_date == null) {
+            if (other.end_date != null)
+                return false;
+        } else if (!end_date.equals(other.end_date))
+            return false;
+        if (family_status == null) {
+            if (other.family_status != null)
+                return false;
+        } else if (!family_status.equals(other.family_status))
+            return false;
+        if (fax == null) {
+            if (other.fax != null)
+                return false;
+        } else if (!fax.equals(other.fax))
+            return false;
+        if (gender == null) {
+            if (other.gender != null)
+                return false;
+        } else if (!gender.equals(other.gender))
+            return false;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        if (img_url == null) {
+            if (other.img_url != null)
+                return false;
+        } else if (!img_url.equals(other.img_url))
+            return false;
+        if (is_current == null) {
+            if (other.is_current != null)
+                return false;
+        } else if (!is_current.equals(other.is_current))
+            return false;
+        if (links == null) {
+            if (other.links != null)
+                return false;
+        } else if (!links.equals(other.links))
+            return false;
+        if (mmms_count == null) {
+            if (other.mmms_count != null)
+                return false;
+        } else if (!mmms_count.equals(other.mmms_count))
+            return false;
+        if (name == null) {
+            if (other.name != null)
+                return false;
+        } else if (!name.equals(other.name))
+            return false;
+        if (number_of_children == null) {
+            if (other.number_of_children != null)
+                return false;
+        } else if (!number_of_children.equals(other.number_of_children))
+            return false;
+        if (party_name == null) {
+            if (other.party_name != null)
+                return false;
+        } else if (!party_name.equals(other.party_name))
+            return false;
+        if (party_url == null) {
+            if (other.party_url != null)
+                return false;
+        } else if (!party_url.equals(other.party_url))
+            return false;
+        if (phone == null) {
+            if (other.phone != null)
+                return false;
+        } else if (!phone.equals(other.phone))
+            return false;
+        if (place_of_birth == null) {
+            if (other.place_of_birth != null)
+                return false;
+        } else if (!place_of_birth.equals(other.place_of_birth))
+            return false;
+        if (place_of_residence == null) {
+            if (other.place_of_residence != null)
+                return false;
+        } else if (!place_of_residence.equals(other.place_of_residence))
+            return false;
+        if (place_of_residence_lat == null) {
+            if (other.place_of_residence_lat != null)
+                return false;
+        } else if (!place_of_residence_lat.equals(other.place_of_residence_lat))
+            return false;
+        if (place_of_residence_lon == null) {
+            if (other.place_of_residence_lon != null)
+                return false;
+        } else if (!place_of_residence_lon.equals(other.place_of_residence_lon))
+            return false;
+        if (residence_centrality == null) {
+            if (other.residence_centrality != null)
+                return false;
+        } else if (!residence_centrality.equals(other.residence_centrality))
+            return false;
+        if (residence_economy == null) {
+            if (other.residence_economy != null)
+                return false;
+        } else if (!residence_economy.equals(other.residence_economy))
+            return false;
+        if (resource_uri == null) {
+            if (other.resource_uri != null)
+                return false;
+        } else if (!resource_uri.equals(other.resource_uri))
+            return false;
+        if (start_date == null) {
+            if (other.start_date != null)
+                return false;
+        } else if (!start_date.equals(other.start_date))
+            return false;
+        if (votes_count == null) {
+            if (other.votes_count != null)
+                return false;
+        } else if (!votes_count.equals(other.votes_count))
+            return false;
+        if (year_of_aliyah == null) {
+            if (other.year_of_aliyah != null)
+                return false;
+        } else if (!year_of_aliyah.equals(other.year_of_aliyah))
+            return false;
+        return true;
+    }
+    
 }
