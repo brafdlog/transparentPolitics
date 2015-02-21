d52416876194d96e4815352e1395de20a3b32481
Jonathan <jonathan@dudamobile.com>
2015-02-21 14:09:02 +0200

    Add servlet to serve the ember html from all non rest paths

---
+++ src/main/java/com/transparent/politics/servlet/EmberServlet.java
@@ -0,0 +1,26 @@
+package com.transparent.politics.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class EmberServlet extends HttpServlet {
+
+	private static final long serialVersionUID = -4869033449765247770L;
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    	String requestURI = request.getRequestURI();
+    	if (requestURI.startsWith("/index.html") ||requestURI.startsWith("/assets") || requestURI.startsWith("/fonts")) {
+    		// Serve static content of the original request
+			getServletContext().getNamedDispatcher("default").forward(request, response);
+    	} else {
+    		// Forward request to the ember html file
+    		request.getRequestDispatcher("/index.html").forward(request, response);
+    	}
+    }
+	
+}
