7fc6bea4624155bb1010e978be788b3de6e7205f
brafdlog <brafdlog@gmail.com>
2015-03-08 10:22:58 +0200

    Update build script

--- build.sh
+++ build.sh
@@ -4,5 +4,21 @@ echo Pulling from git
 git pull
 echo Running maven
 mvn install
+echo Shutting down tomcat
+shutdown_tomcat
+echo Deleting old tomcat dir
+rm -rf ~/tomcat/webapps/ROOT
+mkdir ~/tomcat/webapps/ROOT
 echo Copying war to tomcat dir
-cp /home/ubuntu/transparentPolitics/target/trapol.war /home/ubuntu/tomcat/webapps/ROOT.war
+cp ~/transparentPolitics/target/trapol.war ~/tomcat/webapps/ROOT/trapol.war
+echo Extracting war
+cd ~/tomcat/webapps/ROOT/
+jar xvf ~/tomcat/webapps/ROOT/trapol.war
+echo Deleting war file
+rm -r ~/tomcat/webapps/ROOT/trapol.war
+echo Copy dist file and servlet context.xml to exploded war folder
+cp -r -u ~/transparentPolitics/WebContent/dist ~/tomcat/webapps/ROOT/dist
+cp ~/transparentPolitics/WebContent/WEB-INF/servlet-context.xml ~/tomcat/webapps/ROOT/WEB-INF/
+echo Starting tomcat
+cd ~/tomcat/bin
+sh startup.sh
