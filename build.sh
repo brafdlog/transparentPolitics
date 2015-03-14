aee1b363b63dc03104fd0f5894cbc3d9cd99108c
brafdlog <brafdlog@gmail.com>
2015-03-14 11:40:47 +0200

    Build script fix

--- build.sh
+++ build.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
-echo Start > buidLog 2>&1
+# Overwrite previous log file
+echo Start > buidLog
 # The line below will cause the script to exit if there was an error in any line
-set-e
+set -e
 cd ~/transparentPolitics
 echo Pulling from git
-git pull >> buidLog 2>>&1
+git pull >> buidLog 2>> buidLog
 echo Running maven
-mvn install >> buidLog 2>>&1
+mvn install >> buidLog 2>> buidLog
 echo Building ember
 cd ~/transparentPolitics/trpo/
-ember build >> buidLog 2>>&1
+ember build >> buidLog 2>> buidLog
 echo Shutting down tomcat
-shutdown_tomcat >> buidLog 2>>&1
+shutdown_tomcat >> buidLog 2>> buidLog
 echo Deleting old tomcat dir
 rm -rf ~/tomcat/webapps/ROOT
 mkdir ~/tomcat/webapps/ROOT
@@ -19,7 +20,7 @@ echo Copying war to tomcat dir
 cp ~/transparentPolitics/target/trapol.war ~/tomcat/webapps/ROOT/trapol.war
 echo Extracting war
 cd ~/tomcat/webapps/ROOT/
-jar xvf ~/tomcat/webapps/ROOT/trapol.war >> buidLog 2>>&1
+jar xvf ~/tomcat/webapps/ROOT/trapol.war >> buidLog 2>> buidLog
 echo Deleting war file
 rm -r ~/tomcat/webapps/ROOT/trapol.war
 echo Copy dist file and servlet context.xml to exploded war folder
@@ -27,5 +28,5 @@ cp -r -u ~/transparentPolitics/trpo/dist ~/tomcat/webapps/ROOT/dist
 cp ~/transparentPolitics/WebContent/WEB-INF/servlet-context.xml ~/tomcat/webapps/ROOT/WEB-INF/
 echo Starting tomcat
 cd ~/tomcat/bin
-sh startup.sh >> buidLog 2>>&1
+sh startup.sh >> buidLog 2>> buidLog
 echo Finished!!
